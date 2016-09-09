afbfae871b28a6c3aab4ff88ce6b45b147f1dbec
tbduong <tiffaniduong@gmail.com>
2016-09-09 14:29:37 -0700

    following signup or login users will be redirected to user profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to user_path(current_user[:id])
     else
       redirect_to '/'
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
     end
+  end
+
+  def show
+    @user = User.find_by_id(params)
   end
 
   private
