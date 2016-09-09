bfdfeb7fd4b661e9bb6efd2da5ccc5a11b3a6a83
tbduong <tiffaniduong@gmail.com>
2016-09-09 11:23:18 -0700

    add user new and create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first, :last, :email, :password, :username, :pf_img, :dob, :languages)
+  end
+
 end
