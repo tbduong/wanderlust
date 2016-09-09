3aa527f7f4b56defd6461425fc2de6e7b4e9eb64
tbduong <tiffaniduong@gmail.com>
2016-09-09 11:35:43 -0700

    setup for sessions controller and welcome controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      login(@user)
+      flash[:notice] = "Logged In"
+    else
+      flash[:error] = "Failed to log in"
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
 end
