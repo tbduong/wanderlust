5ba5dd2cc68b5d7265e5196a0a90d59b5d553c3c
tbduong <tiffaniduong@gmail.com>
2016-09-12 20:35:23 -0700

    add location seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+LOCATIONS = [
+   {
+       city: "Seattle",
+       country: "United States"
+   },
+   {
+       city: "Los Angeles",
+       country: "United States"
+   },
+   {
+       city: "New York",
+       country: "United States"
+   },
+   {
+       city: "Miami",
+       country: "United States"
+   },
+   {
+       city: "Paris",
+       country: "France"
+   },
+   {
+       city: "London",
+       country: "United Kingdom"
+   },
+   {
+       city: "Dublin",
+       country: "Ireland"
+   },
+   {
+       city: "Cancun",
+       country: "Mexico"
+   },
+   {
+       city: "Santiago",
+       country: "Chile"
+   },
+   {
+       city: "Vancouver",
+       country: "Canada"
+   },
+   {
+       city: "Rio de Janiero",
+       country: "Brazil"
+   },
+   {
+       city: "Hawaii",
+       country: "United States"
+   },
+   {
+       city: "Patagonia",
+       country: "Chile"
+   },
+   {
+       city: "Buenos Aires",
+       country: "Argentina"
+   }
+ ]
+
+ p Location.create(LOCATIONS)
