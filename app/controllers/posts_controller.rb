af745c15a6be163a5004e0f60109e7175e5687f3
tbduong <tiffaniduong@gmail.com>
2016-09-13 17:07:59 -0700

    remove ransack from index page for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @q = Post.ransack(params[:q])
-    @search = @q.result(distinct: true)
     @posts = Post.all.order(:cached_votes_score => :desc)
     @user = User.find_by_id(params[:id])
+    # @q = Post.ransack(params[:q])
+    # @search = @q.result(distinct: true)
   end
 
   def new
