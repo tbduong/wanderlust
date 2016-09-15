cb578f9a100235bdbaaee11c7fe3566c97a24d1a
tbduong <tiffaniduong@gmail.com>
2016-09-15 01:51:01 -0700

    last min val

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   acts_as_votable
   acts_as_taggable
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :category, presence: true
+
 end
